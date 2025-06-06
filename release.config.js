966c32c2ce7b2d8ce5f3c1b46c4452fc0d3c931b
Pedro Requião <pedrorequiao@Pedros-MacBook-Pro.local>
2025-06-06 12:43:36 -0400

    teste

--- release.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  branches: ['main'],
-  repositoryUrl: 'https://github.com/pedro4r/test-firmware-release.git',
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/github'
-  ]
-};
