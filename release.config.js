8eaefc33ef73977ed7119bf79444a2b2e91712cc
Pedro Requião <pedrorequiao@Pedros-MacBook-Pro.local>
2025-06-06 12:08:45 -0400

    fix: try to trigger proper release

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  repositoryUrl: `https://${process.env.GH_TOKEN}@github.com/pedro4r/test-firmware-release.git`,
   branches: ['main'],
+  repositoryUrl: 'https://github.com/pedro4r/test-firmware-release.git',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
